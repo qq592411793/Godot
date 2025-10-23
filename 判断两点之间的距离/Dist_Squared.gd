b41d766006a199549d76dc91d3799db803fdca3a
abyss233 <68579006+qq592411793@users.noreply.github.com>
2025-10-23 11:09:34 +0800

    Fix distance calculation to use player node position

--- 判断两点之间的距离/Dist_Squared.gd
+++ 判断两点之间的距离/Dist_Squared.gd
@@ -5,6 +5,6 @@ func _process(delta: float) -> void:
   #或者本身节点位置
   var enemy_position = position
   #获取本节点与玩家之间的距离
-  var dist_squared = enemy_position.distance_to(player_position)
+  var dist_squared = enemy_position.distance_to(player_node)
   #打印输出结果
   print(dist_squared)
